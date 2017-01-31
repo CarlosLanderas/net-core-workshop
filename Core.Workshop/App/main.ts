ddbeecaeda20512d832889069ea016445b4d6f6a
Carlos Landeras <carloslanderas@outlook.com>
2017-01-31 22:50:10 +0100

    Clear Files #2

--- Core.Workshop/App/main.ts
+++ Core.Workshop/App/main.ts
@@ -1,15 +1,3 @@
-import axios from "axios";
-
-
-let logo = require("./images/usermessage.png");
-
-let jsonbutton = document.getElementById('jsonbutton');
-jsonbutton.addEventListener('click', ()=> {
-    require.ensure([], require=> {
-        let jsonconfig: any = require('./config/app.config.json');        
-        console.log(jsonconfig.app);
-    });
-});
 
 
 
