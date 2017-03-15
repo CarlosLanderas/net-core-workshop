69c9b730fcb93abdcb0ec9af08e962bcb8d1c65a
Carlos Landeras <carloslanderas@outlook.com>
2017-03-15 20:13:21 +0100

    Add exception handler

---
+++ Core.Workshop/Handlers/HttpExceptionHandler.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Core.Workshop.Handlers
+{
+    public class HttpExceptionHandler
+    {
+        private readonly RequestDelegate next;
+
+        public HttpExceptionHandler(RequestDelegate next)
+        {
+            this.next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            try
+            {
+                await next(context);
+            }
+            catch (Exception exception)
+            {
+                HandleException(context, exception);                
+            }
+        }
+
+        private Task HandleException(HttpContext context, Exception exception)
+        {
+            Debug.WriteLine($"{exception.Message} {exception.InnerException?.Message ?? string.Empty}");
+            return Task.CompletedTask;
+        }
+    }
+}
