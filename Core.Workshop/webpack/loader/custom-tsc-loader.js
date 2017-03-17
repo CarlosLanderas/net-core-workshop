c56481716d510567f60a769d65493589397a8861
Carlos Landeras <carloslanderas@outlook.com>
2017-03-17 21:49:16 +0100

    Add custom tsc loader example

---
+++ Core.Workshop/webpack/loader/custom-tsc-loader.js
@@ -0,0 +1,49 @@
+﻿//usage
+
+//test: /\.ts$/,
+//    loader: './webpack/loadercustom-tsc-loader',
+//query:  {
+//    target: 'es5',
+//    module : 'commonjs'
+//}
+
+const ts = require('typescript');
+
+const getModuleKinds = () => {
+    return {
+        commonjs: 1,
+        amd: 2,
+        umd: 3,
+        system: 4,
+        es2015: 5
+    };
+}
+
+const getTargets = () => {
+    return {
+        es3: 0,
+        es5: 1,
+        es6: 2,
+        es2015: 2,
+        es2016: 3,
+    };
+}
+module.exports = function (content) {
+
+    let targetModule = getModuleKinds()[this.query.module];
+    let target = getTargets()[this.query.target];
+
+    if (!targetModule || !target) {
+        throw Error("Bad module or target configuration");
+    }
+
+    let transpileOptions = {
+        targetModule,
+        target,
+        sourceMap: true
+    };
+
+    let transpiledModule = ts.transpileModule(content, { compilerOptions: transpileOptions });
+    return transpiledModule.outputText;
+}
+
